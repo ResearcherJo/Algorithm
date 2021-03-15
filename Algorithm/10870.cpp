8f49bbb721102a58849818519c88b1e5aec39cb0
JaeYong_Joe <researcherdragon@gmail.com>
2021-03-16 00:00:31 +0900

    3/16

---
+++ Algorithm/10870.cpp
@@ -0,0 +1,23 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+int pibo(int n) {
+	if (n == 2) {
+		return 1;
+	}
+	if (n == 1) {
+		return 1;
+	}
+	if (n == 0) {
+		return 0;
+	}
+	return pibo(n-2)+pibo(n - 1);
+}
+
+int main() {
+	int n;
+	scanf("%d", &n);
+	printf("%d", pibo(n));
+	return 0;
+}
