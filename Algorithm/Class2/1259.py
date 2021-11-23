9301ce751eabc68adb07ab8fa3c1d88488e468b8
조재용 <babnbabn@kakao.com>
2021-11-23 11:12:03 +0000

    21.11.23

---
+++ Algorithm/Class2/1259.py
@@ -0,0 +1,14 @@
+import sys
+
+b=''
+
+while True:
+    a = input()
+    
+    if a =='0':
+        break
+    if a == a[::-1]:
+        print('yes')
+    else:
+        print('no')
+    
---
+++ Algorithm/Class2/1436.py
@@ -0,0 +1,15 @@
+
+
+n = int(input())
+
+count=0
+number=666
+while True:
+    if count==n:
+        break
+    if '666' in str(number):
+        count+=1
+    number+=1
+
+
+print(number-1)
