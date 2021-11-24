19b13c0e319e22b1a46851b5e4df97133dc2def2
조재용 <babnbabn@kakao.com>
2021-11-24 13:21:33 +0000

    21.11.24

---
+++ Algorithm/Theory/Parametric_Search.py
@@ -0,0 +1,38 @@
+'''
+이분탐색 알고리즘을 이야기할 때 함께 나오는 탐색 방법인 매개변수 탐색(Parametric Search)는 이분탐색과 상당히 유사합니다.
+두 개념을 구분하지 않고 이분탐색의 방법으로 매개변수 탐색(Parametric Search)를 소개하기도 합니다.
+다만 차이점이라고 한다면 이분탐색은 원하는 조건을 찾으면 바로 결과값을 반환하고 종료하는 알고리즘이지만 매개변수 탐색(Parametric Search)는 조건을 만족시킨 이후에도 최대, 최소를 찾기 위해 끝까지 탐색을 계속합니다
+'''
+
+#1654 랜선자르기 문제
+
+N, K = map(int,input().split())
+N_list = list()
+
+for i in range(N):
+    N_list.append(int(input()))
+    
+N_list.sort()
+
+
+start = 1
+end = N_list[N-1]
+
+
+mid = (start+end)//2
+key=0
+while start<=end:
+    mid = (start+end)//2
+    count = 0
+    
+    for i in N_list:
+        count+=i//mid
+    
+    if count>=K: #원하는 조건을 만족해도 최대 혹은 최소를 찾기위해 계속 탐색한다. 그러기 위해서 >가 아닌 >=이다.
+        start = mid+1
+        key = mid    
+    else:
+        end = mid-1
+
+
+print(key)
---
+++ Algorithm/Theory/sieve_of_Eratosthenes.py
@@ -0,0 +1,19 @@
+'''
+수학에서 에라토스테네스의 체는 소수를 찾는 방법이다. 고대 그리스 수학자 에라토스테네스가 발견하였다.
+한편, 에라토스테네스의 체를 이용해 1~n까지의 소수를 알고 싶다면, n까지 모든 수의 배수를 다 나눠 볼 필요는 없다. 만약 n보다 작은 어떤 수 m이 m=ab라면 a와 b중 적어도 하나는 n의 제곱근이하이다. 
+즉 n보다 작은 합성수 m은 n의 제곱근 보다 작은 수의 배수만 체크해도 전부 지워진다는 의미이므로, n의 제곱근 이하의 수의 배수만 지우면 된다.
+'''
+
+def prime_list(n):
+    # 에라토스테네스의 체 초기화: n개 요소에 True 설정(소수로 간주)
+    sieve = [True] * n
+
+    # n의 최대 약수가 sqrt(n) 이하이므로 i=sqrt(n)까지 검사
+    m = int(n ** 0.5)
+    for i in range(2, m + 1):
+        if sieve[i] == True:           # i가 소수인 경우
+            for j in range(i+i, n, i): # i이후 i의 배수들을 False 판정
+                sieve[j] = False
+
+    # 소수 목록 산출
+    return [i for i in range(2, n) if sieve[i] == True]
