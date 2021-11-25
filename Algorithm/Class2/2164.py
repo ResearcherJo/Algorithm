333810b65fbdc4177cd7fb4e11d9db09bd629cd0
조재용 <babnbabn@kakao.com>
2021-11-25 11:15:12 +0000

    21.11.25

--- Algorithm/Class2/2164.py
+++ Algorithm/Class2/2164.py
@@ -9,7 +9,6 @@ while len(d)!=1:
     d.append(d.popleft())
 print(d[0])
     
-    
 '''
     아이디어는 맞았는데 pop이라는 함수는 사용이 안되고 사용 안하니 시간초과가 났다. 그래서 from collections import deque이거 찾아서 씀 굿
     https://mong9data.tistory.com/50여기서
