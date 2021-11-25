6840f81bc10e719757a3f449971a25c67ef5a09b
조재용 <babnbabn@kakao.com>
2021-11-25 11:09:41 +0000

    21.11.25

---
+++ Algorithm/Class2/2164.py
@@ -0,0 +1,16 @@
+from collections import deque
+
+N = int(input())
+
+d = deque(i for i in range(1,N+1))
+
+while len(d)!=1:
+    d.popleft()
+    d.append(d.popleft())
+print(d[0])
+    
+    
+'''
+    아이디어는 맞았는데 pop이라는 함수는 사용이 안되고 사용 안하니 시간초과가 났다. 그래서 from collections import deque이거 찾아서 씀 굿
+    https://mong9data.tistory.com/50여기서
+'''
