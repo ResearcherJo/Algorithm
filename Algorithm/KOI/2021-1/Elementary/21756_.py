b2ceabf73a1cdd694d19e00701f76e6d536bb006
조재용 <babnbabn@kakao.com>
2021-12-25 14:54:21 +0000

    21.12.25

--- Algorithm/KOI/2021-1/Elementary/21756_.py
+++ Algorithm/KOI/2021-1/Elementary/21756_.py
@@ -35,5 +35,14 @@ for i in l:
     
     처음 생각 했던 방식이었는데 메모리 때문에 안될줄 알았는데 그냥 할걸 그랬다. 긜고 enumerate도 좀 알아봐야 겠다. 
     사실 필요 없을지도? 그냥 굳이 삭제가 아니라 옮기면 된다는 아이디어를 가지고 있고, 구현할 줄 알았다면...
-
+    
+    n=int(input())
+    x=[item for item in range(1,n+1)]
+    while True:
+        if len(x)==1:
+            break
+        del x[0:len(x):2]
+    print(x[0])
+    
+    삭제를 할려면 이렇게 한번에 del로 삭제 했어야 했다.
 '''
