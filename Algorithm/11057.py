5d5193a0abc5a020c59ea472fae5a6116e74517c
WithJo <researcherdragon@gmail.com>
2022-12-30 16:31:26 +0900

    22.12.30

---
+++ Algorithm/11057.py
@@ -0,0 +1,20 @@
+from sys import stdin, stdout
+
+input = stdin.readline 
+
+array = [[0 for j in range(10)] for i in range(10001)]
+
+n = int(input())
+
+for i in range(10):
+    array[1][i] = 1
+
+for i in range(2,n+1):
+    for j in range(10):
+        if j==0:
+            array[i][j] = array[i-1][j]
+        else:
+            array[i][j] = sum(array[i-1][0:j+1])
+
+
+print(sum(array[n][0:10])%10007)
