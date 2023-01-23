f1e22393f767e382e5c821aaf76afc8da9ec9a6b
WithJo <researcherdragon@gmail.com>
2023-01-23 22:26:58 +0900

    23.01.23

--- Algorithm/10866.py
+++ Algorithm/10866.py
@@ -1,15 +1,14 @@
-
-
 from collections import deque
-import sys
+from sys import stdin
 
+input = stdin.readline
 
 N = int(input())
 
 deq = deque()
 
 for i in range(N):
-    S = sys.stdin.readline()
+    S = input()
     
     if 'push_front' in S:
         S, T = S.split()
@@ -20,34 +19,29 @@ for i in range(N):
         T = int(T)
         deq.append(T)
     elif 'pop_front' in S:
-        if len(deq)==0:
+        if not deq:
             print('-1')
         else:
             print(deq.popleft())
     elif 'pop_back' in S:
-        if len(deq)==0:
+        if not deq:
             print('-1')
         else:
             print(deq.pop())
     elif 'size' in S:
         print(len(deq))
     elif 'empty' in S:
-        if len(deq)==0:
+        if not deq:
             print('1')
         else:
             print('0')
     elif 'front' in S:
-        if len(deq)==0:
+        if not deq:
             print('-1')
         else:
             print(deq[0])
     else:
-        if len(deq)==0:
+        if not deq:
             print('-1')
         else:
             print(deq[-1])
-    
-    
-'''
-    시간초과 났을때는 sys.stdin.readline을 써보자
-'''
