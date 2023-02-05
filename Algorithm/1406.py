d8f5afecbbf0e9982f237a73ec63b05b7081c5d9
WithJo <researcherdragon@gmail.com>
2023-02-05 21:35:12 +0900

    23.02.05

--- Algorithm/1406.py
+++ Algorithm/1406.py
@@ -1,3 +1,28 @@
+from sys import stdin
+
+input = stdin.readline
+
+L = list(input().rstrip())
+R = list()
+
+for _ in range(int(input())):
+    command = list(input().split())
+    if command[0] == 'L':
+            if L: #L이 비어있다는 것은 가장 왼쪽이라는 소리이기 때문에 왼쪽으로 못가게 해야 한다.
+                R.append(L.pop())
+    elif command[0] == 'D':
+        if R: #R이 비어있다는 것은 가장 오른쪽이라는 소리이기 때문에 오른쪽으로 못가게 해야 한다.
+            L.append(R.pop())
+    elif command[0] =='B':
+        if L:
+            L.pop()
+    else:
+        L.append(command[1])
+
+L.extend(reversed(R)) #reverrse가 아닌 reversed인 이유는 R이 비어 있을때 오류가 나지 않기 위해서이다.
+print(''.join(L))
+
+
 '''
 from sys import stdin,stdout
 
@@ -27,55 +52,4 @@ for i in range(n):
             point -=1
     
 print("".join(map(str,array)))
-'''
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-'''
-from sys import stdin
-
-input = stdin.readline
-
-L = list(input().rstrip())
-R = []
-
-for _ in range(int(input())):
-    command = list(input().split())
-    if command[0] == 'L':
-            if L: #L이 비어있다는 것은 가장 왼쪽이라는 소리이기 때문에 왼쪽으로 못가게 해야 한다.
-                R.append(L.pop())
-    elif command[0] == 'D':
-        if R: #R이 비어있다는 것은 가장 오른쪽이라는 소리이기 때문에 오른쪽으로 못가게 해야 한다.
-            L.append(R.pop())
-    elif command[0] =='B':
-        if L:
-            L.pop()
-    else:
-        L.append(command[1])
-
-L.extend(reversed(R)) #reverrse가 아닌 reversed인 이유는 R이 비어 있을때 오류가 나지 않기 위해서이다.
-print(''.join(L))
 '''
--- Algorithm/2745_.py
+++ Algorithm/2745_.py
@@ -17,3 +17,11 @@ print(result)
     새로운 진수에 대한 개념을 알게 되었고, 그걸 이용할 수 있었다. https://growingarchive.tistory.com/208 8을 2진수로 바꿀려면 1000 = (1*2^3)+(0*2^2)+(0*2^1)+(0*2^0) = 8 인것을 이용한것이다.
     진수라는 수학적 개념에 대해 알아야 될 것 같다.
 '''
+
+from sys import stdin
+
+input = stdin.readline
+
+n, b = input().split()
+
+print(int(n,int(b)))
