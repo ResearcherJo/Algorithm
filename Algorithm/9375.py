9c19574ea969f87f8a647f19ac551303220c6889
WithJo <researcherdragon@gmail.com>
2023-04-02 21:20:32 +0900

    23.04.02

--- Algorithm/9375.py
+++ Algorithm/9375.py
@@ -2,18 +2,18 @@ from sys import stdin
 
 input = stdin.readline
 
-t = int(input())
+t = int(input()) #테스트케이스 입력
 
 for _ in range(t):
     n = int(input())
     dic = dict()
     for i in range(n):
         c, ty = input().split()
-        if ty in dic:
+        if ty in dic: #입력받은 옷의 종류가 있는 종류라면 +1을 한다.
             dic[ty] += 1
-        else:
+        else: #입력받은 옷의 종류가 없는 종류라면 2로 저장한다. (옷의 개수 1 + 선택안할경우 1)
             dic[ty] = 2
-    #print(dic)
+    
     sum =1
     for j in dic:
         sum *= dic[j]
