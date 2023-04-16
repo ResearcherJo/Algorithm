1a855567421569b1c33cf55bbcf51215f3ae7740
choojaeyong <researcherdragon2002@gmail.com>
2023-04-16 16:50:53 +0900

    23.04.16

--- Algorithm/1654_.py
+++ Algorithm/1654_.py
@@ -1,39 +1,51 @@
+from sys import stdin
 
+input = stdin.readline
 
-N, K = map(int,input().split())
-N_list = list()
+def Iterative(array,n):
+    #start는 1, end는 가장 긴 랜선의 길이로 설정한다.
+    start = 1
+    end = max(array)
+    m = 0
 
-for i in range(N):
-    N_list.append(int(input()))
+    while start<=end: #끝까지 탐색해보자.
+        count = 0
+        mid = (start+end)//2 #mid길이로 잘랐을때를 생각한다.
+
+        #count에 mid로 잘랐을때 나오는 랜선의 개수를 저장
+        for i in array:
+            count+=i//mid
+
+        #count가 구하려는 n개보다 크다면
+        if count>=n:
+            start = mid+1 #mid보다 긴 길이도 탐색해본다.
+            m = mid #조건에 충족하는 길이 mid를 m에 저장한다.
+        else:
+            end = mid-1 #count가 n보다 작으면 길이를 줄여보자.
+
+    return m
+
+def recursive(array,start,end,n):
+    if start>end: #start 가 end보다 크다면 end를 반환한다.
+        return end
     
-N_list.sort()
-
-
-start = 1
-end = N_list[N-1]
-
-
-mid = (start+end)//2
-key=0
-while start<=end:
-    mid = (start+end)//2
     count = 0
-    
-    for i in N_list:
+    mid = (start+end)//2
+
+    #count에 mid로 잘랐을때 나오는 랜선의 개수를 저장
+    for i in array:
         count+=i//mid
-    
-    if count>=K:
-        start = mid+1
-        key = mid
-    else:
-        end = mid-1
+
+    if count>=n: #count가 구하려는 n개보다 크다면
+        return recursive(array,mid+1,end,n)#mid보다 긴 길이도 탐색해본다.
+    else: #count가 n보다 작으면 길이를 줄여보자.
+       return recursive(array,start,mid-1,n)
 
 
-print(key)
 
-'''
-    이분탐색(Binary Search)에 더한 매개변수탐색(Parametric Search)을 이용
-    굳이 리스트를 활용할 필요는 없다. 오히려 메모리 초과를 불러온다.
-    https://claude-u.tistory.com/443에서는 end가 정답이라고 했는데 그 이유는 아직 모르겠다.
-    이분탑색할때 범위를 잘 고려해야 한다.
-'''
+k, n = map(int,input().split())
+array = [int(input())for _ in range(k)]
+
+
+#print(recursive(array,1,max(array),n))
+print(Iterative(array,n))
