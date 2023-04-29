f8a91471dfa3ff24bfeae582030ea8f40c66fa7b
choojaeyong <researcherdragon2002@gmail.com>
2023-04-29 21:43:40 +0900

    23.04.29 / 절댓값 힙(힙)

--- Algorithm/11286.py
+++ Algorithm/11286.py
@@ -2,57 +2,58 @@ from sys import stdin
 
 input = stdin.readline
 
-heap = [999] + list()
+def insert(heap, key):
+    global size
+    size += 1
+    current = size
 
-
-def heappush(n):
-    heap.append(n)
-
-    current = len(heap)-1 #가장 마지막 값의 index임
-
-    while current>1:#부모가 있을 때까지
-        if abs(heap[current])<=abs(heap[current//2]):#current가 부모보다 작다면 부모랑 값을 바꾸자.
-            if abs(heap[current//2])>abs(heap[current]) or heap[current]<=heap[current//2]:
-                heap[current], heap[current//2] = heap[current//2], heap[current]
-                current//=2#부모를 current로 
-        else:
+    while current!=1 and abs(key)<=abs(heap[current//2]):
+        if abs(key)==abs(heap[current//2]) and key>heap[current//2]:
             break
-            
-def heappop():
-    if len(heap)==1: #비어있으니 그냥 0출력
-        return '0'
-    elif len(heap)==2: # heap에 1개밖에 없으니 출력하고 리턴
-        return heap.pop()
+        heap[current] = heap[current//2]
+        current//=2
 
-    current = 1 # 1에서 부터 찾을 것이다.
-    child = 2 #자식은 2부터 찾을 것이다.
+    heap[current] = key
 
-    temp = heap[1]# root를 반환값으로 temp에 저장
-    heap[current] = heap.pop() #root에 가장 마지막 값을 넣는다.
+def delete(heap):
+    global size
+    if size==0:
+        return 0
+    
+    item = heap[1]
 
-    while child<len(heap): #자식이 있다면 반복
+    temp = heap[size]
+    size-=1
+    parent = 1
+    child = 2
 
-        if child+1<len(heap) and abs(heap[child])>=abs(heap[child+1]):#오른쪽 자식이 있고, 오른쪽 자식이 왼쪽 자식보다 작다면 오른쪽 자식을 가리키게
-            if abs(heap[child])>abs(heap[child+1]) or heap[child]>=heap[child+1]:
-                child += 1
-            
-        if abs(heap[current])>=abs(heap[child]):
-            if heap[current] >= heap[child] or abs(heap[current])>abs(heap[child]):#current값이 자식 값보다 크다면 자식 값과 바꾸고, current가 자식을 바라보게 한다. child는 current의 지삭을 본다.
-                heap[current], heap[child] =heap[child], heap[current]
-                current = child
-                child *=2
-        else:
+    while child <= size:
+        if child < size and abs(heap[child])>abs(heap[child+1]):
+            child +=1
+        elif child < size and abs(heap[child])==abs(heap[child+1]) and heap[child]>heap[child+1]:
+            child +=1
+        
+        if abs(temp)<abs(heap[child]):
+            break
+        elif abs(temp)==abs(heap[child]) and temp<heap[child]:
             break
 
-    return temp
+        heap[parent] = heap[child]
+        parent = child
+        child *=2
 
+    heap[parent] = temp
+    return item
 
 n = int(input())
+size = 0
+heap = [0]*100000
+
 
 for i in range(n):
-    x = int(input())
-    if x:
-        heappush(x)
+    key = int(input())
+
+    if key==0:
+        print(delete(heap))
     else:
-        print(heappop())
-    #print(*heap)
+        insert(heap,key)
