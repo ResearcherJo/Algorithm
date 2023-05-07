158179349c1a5691c415a5432300519d8fd6ce80
WithJo <researcherdragon@gmail.com>
2023-05-07 21:08:46 +0900

    23.05.07 / 회의실 배정(그리디)

--- Algorithm/1931.py
+++ Algorithm/1931.py
@@ -1,21 +1,19 @@
-from sys import stdin, stdout
+from sys import stdin
 
 input = stdin.readline
 
+array = [list(map(int,input().split())) for _ in range(int(input()))]
+count = 0#회의 횟수
+fin = 0 #끝나는 시간
 
-n = [list(map(int,input().split())) for _ in range(int(input()))]
+#빨리 끝나는 순으로 정렬
+array.sort(key=lambda x : x[0])
+array.sort(key=lambda x : x[1])
 
-n.sort(key = lambda x : x[0])
-n.sort(key = lambda x : x[1])
-
-
-count=0
-key = -1
-
-for i in n:
-    if key<=i[0]:
-        key = i[1]
+for key in array:
+    #시작하는 시간이 끝나는 시간후라면 그 회의가 회의실을 사용한다. 끝나는 시간을 업데이트 한다.
+    if key[0]>=fin:
+        fin = key[1]
         count+=1
-        
-
+    
 print(count)
