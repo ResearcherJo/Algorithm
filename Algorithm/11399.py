3583a48abb85e0b4228e7258e1a5f6e5cca387e7
choojaeyong <researcherdragon2002@gmail.com>
2023-05-08 12:50:05 +0900

    23.05.08 / ATM(그리디)

--- Algorithm/11399.py
+++ Algorithm/11399.py
@@ -1,13 +1,16 @@
+from sys import stdin
 
+input = stdin.readline
 
+N  = int(input())
+arr = list(map(int,input().split()))
+s = 0
 
-n = int(input())
+#오름차순으로 정렬
+arr.sort()
 
-nl = list(map(int,input().split()))
+#정렬된 값을 더한다.
+for i in range(1,N+1):
+    s+=sum(arr[0:i])
 
-nl.sort()
-
-for i in range(1,n):
-    nl[i] = nl[i]+nl[i-1]
-
-print(sum(nl))
+print(s)
